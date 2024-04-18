f3df0d04ad08b8ccea0ddb49ea7b006bf487086f
Milind <m.milind15@gmail.com>
2024-04-18 21:21:43 +0530

    idk

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -87,6 +87,11 @@ const Users: React.FC<UsersProps> = ({ users, currentUser, setUsers }) => {
                         return u;
                       });
 
+
+                      console.log(currentUser.username, "followed", user.username)
+                      console.log("Old users", users);
+                      console.log("New users", newUsers);
+
                       setUsers([...newUsers]);
                     }}
                     className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
